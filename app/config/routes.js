95554ecc0fa07aee3e36cdefb6a3103445d4c78d
Noah Springwater <nspringwater@gmail.com>
2016-06-09 18:44:25 -0400

    routers

---
+++ app/config/routes.js
@@ -0,0 +1,25 @@
+var React = require('react');
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var IndexRoute = ReactRouter.IndexRoute;
+var hashHistory = ReactRouter.hashHistory;
+var Main = require('../components/Main');
+var Home = require('../components/Home');
+
+var routes = (
+  <Router history={hashHistory}>
+    <Route path='/' component={Main}>
+      <IndexRoute component={Home} />
+    </Route>
+  </Router>
+);
+
+module.exports = routes;
+
+
+
+// <Route path='playerOne' header='Player One' component={PromptContainer} />
+// <Route path='PlayerTwo' header='PlayerTwo/:playerOne' component={PromptContainer} />
+// <Route path='battle' component={ConfirmBattleContainer} />
+// <Route path='results' component={ResultsContainer} />
--- app/index.js
+++ app/index.js
@@ -1,44 +1,8 @@
-var USER_DATA = {
-  name: 'Noah Springwater',
-  username: 'noah-springwater'
-}
 var React = require('react');
 var ReactDOM = require('react-dom');
-
-var ProfileLink = React.createClass({
-  render: function () {
-    return (
-      <div>
-        <a href={'https://www.github.com/' + this.props.username} target='_blank'>
-          {this.props.username}
-        </a>
-      </div>
-    )
-  }
-});
-
-var ProfileName = React.createClass({
-  render: function () {
-    return (
-      <div>{this.props.name}</div>
-    )
-  }
-});
-
-var Avatar = React.createClass({
-  render: function () {
-    return (
-      <div>
-        <ProfileLink username={this.props.user.username}/>
-        <ProfileName name={this.props.user.name}/>
-      </div>
-    )
-  }
-})
+var routes = require('./config/routes')
 
 ReactDOM.render(
-  <Avatar user={USER_DATA} />,
+  routes,
   document.getElementById('app')
 );
-
-saf
