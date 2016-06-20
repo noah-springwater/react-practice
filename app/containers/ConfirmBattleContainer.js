7699ccfd624ea6646f54182e364690b3833aea5f
Noah Springwater <nspringwater@gmail.com>
2016-06-20 17:51:22 -0400

    battle page

---
+++ app/containers/ConfirmBattleContainer.js
@@ -0,0 +1,36 @@
+var React = require('react');
+var ConfirmBattle = require('../components/ConfirmBattle');
+var githubHelpers = require('../utils/githubHelpers');
+
+var ConfirmBattleContainer = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+  getInitialState: function () {
+    console.log('getInitialState');
+    return {
+      isLoading: true,
+      playerInfo: []
+    }
+  },
+  componentDidMount: function () {
+    var query = this.props.location.query;
+    //fetch info from github then update state
+    githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
+      .then(function (players) {
+        this.setState({
+          isLoading: false,
+          playersInfo: [players[0], players[1]]
+        })
+      }.bind(this))
+  },
+  render: function () {
+    return(
+      <ConfirmBattle
+        isLoading={this.state.isLoading}
+        playersInfo={this.state.playersInfo} />
+    );
+  }
+});
+
+module.exports = ConfirmBattleContainer;
